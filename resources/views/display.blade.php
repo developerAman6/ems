6128eba7f2f22f439b64fd2bf263df188bd85717
developeraman6 <amana@gmail.com>
2021-05-26 14:30:08 +0530

    first commit

---
+++ resources/views/display.blade.php
@@ -0,0 +1,43 @@
+<!doctypte html>
+<html lang="en">
+<head>
+  <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+  <meta charset="utf-8">
+  <title>Enquiry</title>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body class="container">
+  <div>
+    <h2> YAMAHA SHOWROOM | SK MOTORS | CHHATARPUR | GWALIOR</h2>
+    <h3>TO THE MOON</h3>
+    <hr>
+    <H4><a href ="/">HOME</a> | <a href ="/aboutus">ABOUT US</a> | <a href ="/contactus">CONTACT US</a> | <a href ="/products">OUR PRODUCTS</a></H4>
+    <hr>
+    @yield('content')
+  </div>
+  <hr>
+  <H1>REACH US</h1>
+  <h3>SK MOTOS IS NOW AVAILABLE IN GWALIOR AND CHHATARPUR</H3>
+  <H3>Feel free to contact :</h3>
+  <h4>GWALIOR : Sachin tendulkar marg, City center, Gwalior - 474001</h4>
+  <h4>CHHATARPUR : Jhansi road, Chhatarpur - 471001</h4>
+  <h4>CONTACT : 7987175381 | 9425141014</h4>
+
+</body>
+</html>
